b5fac512c63332c96921984ce0d8dc46eb1c9127
Lautaro <85114590+lauta2308@users.noreply.github.com>
2023-03-22 18:37:45 -0300

    Index form validations
    Added validations to login and register form and improved styles

--- src/main/resources/static/web/scripts/main.js
+++ src/main/resources/static/web/scripts/main.js
@@ -7,8 +7,12 @@ createApp({
         loginEmail: "",
         loginPassword: "",
         registerNick: "",
-        registerEmail: "",
-        registerPassword: ""
+        showLogInForm: true,
+        showSignUpForm: false,
+        errorNickName: "",
+        errorLoginEmail: "",
+        errorLoginPassword: "",
+        errorLogin: "",
 
       }},
       created(){
@@ -21,44 +25,120 @@ createApp({
 
       },
       methods: {
+       
+        displayLogInForm(){
+          this.showLogInForm = true;
+          this.showSignUpForm = false;
+
+        },
+
+       displaySignUpForm(){
+        this.showLogInForm = false;
+        this.showSignUpForm = true;
+       },
+
+       resetRegisterNickError(){
+          this.errorNickName = "";
+       },
+       resetLoginEmailError(){
+        this.errorLoginEmail = "";
+        this.errorLogin = "";
+       },
+
+       checkRegisterNick(){
+        if(this.registerNick.length === 0){
+          this.errorNickName = "Write a NickName";
+        } else {
+          this.errorNickName = "";
+        }
+       },
+
+       checkLoginEmail(){
+        if(this.loginEmail.length === 0){
+          this.errorLoginEmail = "Write an email"
+        } else if(
+          !this.loginEmail.includes("@")
+        ) {
+          this.errorLoginEmail = "Email should contain @"
+        } else if(!this.loginEmail.includes(".com")){
+          this.errorLoginEmail = "Not valid email"
+        } 
+        
+        
+        
+        else{
+          this.errorLoginEmail = "";
+        }
+
+       },
+
+       resetLoginPasswordError(){
+          this.errorLoginPassword = "";
+          this.errorLogin = "";
+       },
+
+       checkLoginPassword(){
+        if(this.errorLoginPassword.length === 0){
+          this.errorLoginPassword = "Write a password";
+        }
+
+       },
+
+  
 
         login() {
-        
+           if(this.loginEmail.length === 0 && this.loginPassword.length === 0){
+            this.errorLogin = "Complete all fields to log in";
+          }
+
+          else if(this.loginEmail.length === 0 || this.loginPassword.length === 0){
+            this.errorLogin = "Complete all fields to log in";
+          } else{
             axios.post('/api/login', `email=${this.loginEmail}&password=${this.loginPassword}`).then(response => {
 
-                console.log(response);
+              console.log(response);
 
-                if(response.status === 200){
-                  axios.patch('/api/clients/logged').then(response => {
-                    this.loginEmail = "",
-                    this.loginPassword = "",
-                    this.registerNick = "",
-                    this.registerEmail = "",
-                    this.registerPassword = ""
-                      window.location.href="./web/mainchat.html";
-                      }
-                      )
-                }
+              if(response.status === 200){
+                axios.patch('/api/clients/logged').then(response => {
+                  this.loginEmail = "",
+                  this.loginPassword = "",
+                  this.registerNick = "",
+                  this.registerEmail = "",
+                  this.registerPassword = ""
+                    window.location.href="./web/mainchat.html";
+                    }
+                    )
+              }
+      
+           
+              
+      
+          })
+          }
         
-             
-                
-        
-            })},
+            },
 
             register(){
 
+              if(this.loginEmail.length === 0 && this.loginPassword.length === 0){
+                this.errorLogin = "Complete all fields to Sign Up";
+              }
+    
+              else if(this.loginEmail.length === 0 || this.loginPassword.length === 0){
+                this.errorLogin = "Complete all fields to Sign Up";
+              } else {
 
-              axios.post('/api/register', `nickName=${this.registerNick}&email=${this.registerEmail}&password=${this.registerPassword}`).then(response => {
+                axios.post('/api/register', `nickName=${this.registerNick}&email=${this.loginEmail}&password=${this.loginPassword}`).then(response => {
         
 
-                this.loginEmail = this.registerEmail;
-                this.loginPassword = this.registerPassword;
 
-                this.login();
-      
-                
+                  this.login();
         
-            })
+                  
+          
+              })
+              }
+
 
             }
 
@@ -76,9 +156,3 @@ createApp({
 
 
 
-
-
-
-
-
-   // axios.post('/api/clients', `nickName=${"lauta1010"}&email=${"lauta1010@test.com"}&password=${"12345"}`)
